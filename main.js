4e70c10632e839bf68a98c69594a3a065df0b7ee
angelinahan <han.ang@northeastern.edu>
2022-02-10 12:46:13 -0500

    Show random number on button1click and change circle color on button2 click

--- main.js
+++ main.js
@@ -7,8 +7,18 @@ Modified: 12/06/21
 */
 
 //Starter code for Button 1 click
-    // You will need to call this function when Button 1 is clicked
-    // You will need to modiffy the body of this function as described in the assignment Readme
+// You will need to call this function when Button 1 is clicked
+// You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+  document.getElementById("random_num").innerHTML = Math.random();
 }
+
+button2Clicked = () => {
+  const circle = document.getElementById("circle");
+  const color = circle.getAttribute("class");
+  if (color === "blue") {
+    circle.setAttribute("class", "green");
+  } else {
+    circle.setAttribute("class", "blue");
+  }
+};
